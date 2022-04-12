e246aac43e56d54935b5c59b7b5ceb0b90b447a8
grazielbarbosa <grazielco@gmail.com>
2022-04-11 22:28:29 -0300

    Projetct Movie.API

---
+++ SecureMicroservices/Movie.API/Data/MovieAPIContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Movies.API.Data
+{
+    public class MovieAPIContext : DbContext
+    {
+        public MovieAPIContext (DbContextOptions<MovieAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Movies.API.Models.Movie> Movie { get; set; }
+    }
+}
